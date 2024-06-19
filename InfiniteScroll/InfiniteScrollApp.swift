281c8c9d1d728f5e025e8135e5beb17a089f4355
Micheal S. Bingham <micheal.s.bingham@icloud.com>
2024-06-19 15:21:23 -0400

    Initial Commit

---
+++ InfiniteScroll/InfiniteScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollApp.swift
+//  InfiniteScroll
+//
+//  Created by Micheal Bingham on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
